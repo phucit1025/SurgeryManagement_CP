a2a4eccba21768def4247771a8c370439cd96041
cvin59 <cvin59@gmail.com>
2019-02-19 12:48:20 +0700

    commit postop

---
+++ Surgery_1/Surgery_1.Services/Interfaces/IPostOpService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surgery_1.Services.Interfaces
+{
+    public interface IPostOpService
+    {
+        object GetSurgeryByStatusId(int statusId);
+        object GetHealthCareRerportBySurgeryShiftId(int surgeryShiftId);
+
+    }
+}
