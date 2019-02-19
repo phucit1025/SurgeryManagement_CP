93a6b7216cc307bb99437d9867c8cf063506955d
HOANH-TUNG\mastermind <hoanhtung>
2019-02-19 19:04:03 +0700

    update make schedule

--- Surgery_1/Surgery_1.Services/Implementations/SurgeryService.cs
+++ Surgery_1/Surgery_1.Services/Implementations/SurgeryService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Surgery_1.Data.Context;
+using Surgery_1.Data.Entities;
 using Surgery_1.Data.ViewModels;
 using Surgery_1.Repositories.Interfaces;
 using Surgery_1.Services.Interfaces;
@@ -32,26 +33,28 @@ namespace Surgery_1.Services.Implementations
         }
         public string GetRoomByMaxSurgeryTime(ScheduleViewModel scheduleViewModel)
         {
+            GetSurgeryShiftNoScheduleByDay();
             //sau này EstimatedTime sẽ đổi thành ActualTime
             //list những phòng có thời gian phẫu thuật trễ nhất, giảm dần
-            DateTime estimatedDate = scheduleViewModel.StartAMWorkingHour.Date;
+            //DateTime estimatedDate = scheduleViewModel.StartAMWorkingHour.Date;
 
-            var result = _context.SurgeryShifts
-                .FromSql("select top 1 max(ActualEndDatetime) as [Datetime], SurgeryRoomId " +
-                            "from SurgeryShifts " +
-                            "where cast(ActualStartDatetime as date) = {0} " +
-                            "group by SurgeryRoomId", estimatedDate).ToList();
-            TimeSpan hour = TimeSpan.FromHours(scheduleViewModel.ExpectedSurgeryDuration);
-            //Đổi estimasted thành result. j đó
-            DateTime endEstimatedTime = estimatedDate + hour;
-            if (endEstimatedTime <= scheduleViewModel.EndAMWorkingHour) //buổi sáng
-            {
-                InsertDateTimeToSurgeryShift(scheduleViewModel.SurgeryShiftId, estimatedDate, endEstimatedTime);
-            } else if (endEstimatedTime <= scheduleViewModel.EndPMWorkingHour) //buổi chiều
-            {
-                InsertDateTimeToSurgeryShift(scheduleViewModel.SurgeryShiftId, estimatedDate, endEstimatedTime);
-            }
-            return scheduleViewModel.StartAMWorkingHour.ToString();
+            //var result = _context.SurgeryShifts
+            //    .FromSql("select top 1 max(EstimatedEndDateTime) as [Datetime], SurgeryRoomId " +
+            //                "from SurgeryShifts " +
+            //                "where cast(EstimatedStartDateTime as date) = {0} " +
+            //                "group by SurgeryRoomId", estimatedDate).ToList();
+            //TimeSpan hour = TimeSpan.FromHours(scheduleViewModel.ExpectedSurgeryDuration);
+            ////Đổi estimasted thành result. j đó
+            //DateTime endEstimatedTime = estimatedDate + hour;
+            //if (endEstimatedTime <= scheduleViewModel.EndAMWorkingHour) //buổi sáng
+            //{
+            //    InsertDateTimeToSurgeryShift(scheduleViewModel.SurgeryShiftId, estimatedDate, endEstimatedTime);
+            //}
+            //else if (endEstimatedTime <= scheduleViewModel.EndPMWorkingHour) //buổi chiều
+            //{
+            //    InsertDateTimeToSurgeryShift(scheduleViewModel.SurgeryShiftId, estimatedDate, endEstimatedTime);
+            //}
+            return "";
         }
         public void InsertDateTimeToSurgeryShift(int surgeryId, DateTime startTime, DateTime endTime)
         {
@@ -65,6 +68,32 @@ namespace Surgery_1.Services.Implementations
         {
             return 1;
         }
-
+        //TODO: Lấy những ca mổ chưa lên lịch theo ngày
+        public List<SurgeryShift> GetSurgeryShiftNoScheduleByDay()
+        {
+            var surgeryShifts = _context.SurgeryShifts.ToList();
+            //    .Where(s => (s.IsAvailableMedicalSupplies == true) && (s.SurgeryRoomId == null)).ToList();
+            var result = _context.SurgeryShifts
+                .FromSql("select StartAMWorkingHour, EndAMWorkingHour, " +
+                "StartPMWorkingHour, EndPMWorkingHour, ExpectedSurgeryDuration, " +
+                "IsAvailableMedicalSupplies, PriorityNumber from SurgeryShifts " +
+                "where IsAvailableMedicalSupplies = {0} and SurgeryRoomId is null", 1);
+            return surgeryShifts;   
+        }
+        //TODO: Import file
+        public void insertFileToSurgeryShift(ScheduleViewModel scheduleViewModel)
+        {
+            var surgeryShift = new SurgeryShift
+            {
+                StartAMWorkingHour = scheduleViewModel.StartAMWorkingHour,
+                EndAMWorkingHour = scheduleViewModel.EndAMWorkingHour,
+                StartPMWorkingHour = scheduleViewModel.StartPMWorkingHour,
+                EndPMWorkingHour = scheduleViewModel.EndPMWorkingHour,
+                ExpectedSurgeryDuration = scheduleViewModel.ExpectedSurgeryDuration,
+                PriorityNumber = scheduleViewModel.PriorityNumber
+            };
+            _context.SurgeryShifts.Add(surgeryShift);
+            _context.SaveChanges();
+        }
     }
 }
