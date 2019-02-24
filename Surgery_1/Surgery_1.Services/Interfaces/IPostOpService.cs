d26f5eb4f968b4905cab3824f0bc3249c64f0553
cvin59 <cvin59@gmail.com>
2019-02-24 13:05:41 +0700

    update postop function

--- Surgery_1/Surgery_1.Services/Interfaces/IPostOpService.cs
+++ Surgery_1/Surgery_1.Services/Interfaces/IPostOpService.cs
@@ -8,7 +8,11 @@ namespace Surgery_1.Services.Interfaces
     public interface IPostOpService
     {
         ICollection<SurgeryShiftViewModel> GetSurgeryByStatusId(int statusId);
+        RecoverySurgeryShiftViewModel GetRecoverySurgeryShiftById(int id);
         object GetHealthCareRerportBySurgeryShiftId(int surgeryShiftId);
         bool ChangeSurgeryShiftToRecovery(int surgeryShiftId);
+        bool CreateHealthCareReport(HealthCareReportViewModel healthCareRerpotViewModel);
+        bool UpdateHealthCareReport(HealthCareReportViewModel healthCareRerpotViewModel);
+        bool SoftDeleteHealthCareReport(int healthCareReportId);
     }
 }
--- Surgery_1/Surgery_1/Controllers/PostOpController.cs
+++ Surgery_1/Surgery_1/Controllers/PostOpController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Surgery_1.Data.ViewModels;
 using Surgery_1.Services.Interfaces;
 
 namespace Surgery_1.Controllers
@@ -38,6 +39,61 @@ namespace Surgery_1.Controllers
                 return NotFound();
             }
             return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult ChangeSurgeryShiftToRecovery(int surgeryShiftId)
+        {
+            var result = _postOpService.ChangeSurgeryShiftToRecovery(surgeryShiftId);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public IActionResult GetRecoverySurgeryShiftById(int surgeryShiftId)
+        {
+            var result = _postOpService.GetRecoverySurgeryShiftById(surgeryShiftId);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateHealthCareReport(HealthCareReportViewModel healthCareReportViewModel)
+        {
+            var result = _postOpService.CreateHealthCareReport(healthCareReportViewModel);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateHealthCareReport(HealthCareReportViewModel healthCareReportViewModel)
+        {
+            var result = _postOpService.UpdateHealthCareReport(healthCareReportViewModel);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public IActionResult SoftDeleteHealthCareReport(int healthCareReportId)
+        {
+            var result = _postOpService.SoftDeleteHealthCareReport(healthCareReportId);
+            if (result)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
         }
     }
 }
