4d0ae1b0049c1f9cf405f983a4520698b62699bb
cvin59 <cvin59@gmail.com>
2019-04-30 10:57:06 +0700

    statistic

---
+++ Surgery_1/Surgery_1.Data/ViewModels/StatisticViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Surgery_1.Data.ViewModels
+{
+    public class StatisticViewModel
+    {
+        public int number;
+        public string specialtyName;
+    }
+
+    public class RoomStatisticViewModel
+    {
+        public double number;
+        public string roomName;
+    }
+}
